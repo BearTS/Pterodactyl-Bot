02bacd59695cd0211bf7881c7461f5fd454d2754
BearTS <bear@tamako.tech>
2020-12-25 01:31:09 +0530

    Defining embederror

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ console.log('Starting the bot...');
 const { Token } = require("./settings.json");
 const { readdirSync } = require('fs');
 const { join } = require('path');
-const { Intents } = require('discord.js');
+const settings = require("./settings.json");
+const { Intents, MessageEmbed } = require('discord.js');
 const Client = require('./struct/Client');
 const tamako = new Client({
 	clientSettings: {
@@ -36,6 +37,24 @@ for (const dir of tamako.config.commanddir) {
 	for (const file of readdirSync(join(__dirname, 'commands', dir)).filter(f => f.split('.').pop() === 'js'))
 		tamako.commands.add(require(`./commands/${dir}/${file}`));
 }
+
+tamako.embederror = function (text) {
+    const embed = new MessageEmbed()
+        .setTitle("Error")
+        .setDescription(text)
+        .setColor(settings.embed.color.error)
+        .setFooter(settings.embed.footer)
+        .setTimestamp();
+    return embed;
+};
+tamako.noperm = function (text) {
+    const embed = new MessageEmbed()
+        .setTitle(`${text} you don't have permission to use this command.`)
+        .setColor(settings.embed.color.error)
+        .setFooter(settings.embed.footer)
+        .setTimestamp();
+    return embed;
+};
 
 for ( const event of readdirSync(join(__dirname, 'events')).filter( f => f.split('.').pop() === 'js'))
   tamako.on(event.split('.')[0], require(`./events/${event}`).bind(null, tamako));
